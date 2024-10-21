7c9eeebc2482c2c9061388eeb5729caa68a7a0e6
Koroem <conduratvictor@yahoo.com>
2024-10-21 12:29:50 +0300

    generate book templates

--- web-components/book-generator-template-modal/book-generator-template-modal.js
+++ web-components/book-generator-template-modal/book-generator-template-modal.js
@@ -1,4 +1,4 @@
-const applicationModule=require('assistos').loadModule('application',{});
+const applicationModule = require('assistos').loadModule('application', {});
 
 export class BooksGeneratorTemplateModal {
     constructor(element, invalidate) {
@@ -18,16 +18,17 @@ export class BooksGeneratorTemplateModal {
     }
 
     async generateBookTemplate(_target) {
-        const formElement = this.element.querySelector("form");
-        const formData = await assistOS.UI.extractFormInformation(formElement);
-        if (!formData.isValid) {
-            return assistOS.UI.showApplicationError("Invalid form data", "Please fill all the required fields", "error");
-        }
-        const documentData = formData.data;
-        debugger
-        const flowTaskId = await applicationModule.runApplicationFlow(assistOS.space.id, "BooksGenerator", "GenerateTemplate", documentData);
-        assistOS.UI.closeModal(_target);
-        await assistOS.UI.changeToDynamicPage(`space-application-page`, `${assistOS.space.id}/Space/document-view-page/${docId}`);
+        await assistOS.loadifyFunction(async () => {
+                const formElement = this.element.querySelector("form");
+                const formData = await assistOS.UI.extractFormInformation(formElement);
+                if (!formData.isValid) {
+                    return assistOS.UI.showApplicationError("Invalid form data", "Please fill all the required fields", "error");
+                }
+                const documentData = formData.data;
+                const documentId = (await applicationModule.runApplicationFlow(assistOS.space.id, "BooksGenerator", "GenerateTemplate", documentData)).data;
+                assistOS.UI.closeModal(_target);
+                await assistOS.UI.changeToDynamicPage(`space-application-page`, `${assistOS.space.id}/Space/document-view-page/${documentId}`);
+            }
+        )
     }
-
 }
