cd20027cb87c8182dac7bc83920641d40e6c1a62
Koroem <conduratvictor@yahoo.com>
2024-11-05 16:32:46 +0200

    updates

--- web-components/books-generator-modal/books-generator-modal.js
+++ web-components/books-generator-modal/books-generator-modal.js
@@ -1,49 +1,5 @@
-/*const applicationModule = require('assistos').loadModule('application', {});
-
-export class BooksGeneratorModal {
-    constructor(element, invalidate) {
-        this.element = element;
-        this.invalidate = invalidate;
-        this.documentId = this.element.getAttribute("data-documentId")
-        this.invalidate();
-
-    }
-
-    async beforeRender() {
-    }
-
-    async afterRender() {
-    }
-
-    async closeModal(_target) {
-        await assistOS.UI.closeModal(_target);
-    }
-
-    async generateBook(_target) {
-        await assistOS.loadifyFunction(async () => {
-                const formElement = this.element.querySelector("form");
-                const formData = await assistOS.UI.extractFormInformation(formElement);
-                if (!formData.isValid) {
-                    return assistOS.UI.showApplicationError("Invalid form data", "Please fill all the required fields", "error");
-                }
-                const {llm, size} = formData.data;
-                const bookGenerationData = {
-                    llm,
-                    size,
-                    documentId: this.documentId
-                }
-                const documentId = (await applicationModule.runApplicationFlow(assistOS.space.id, "BooksGenerator", "GenerateBook", bookGenerationData)).data;
-                assistOS.UI.closeModal(_target);
-                await assistOS.UI.changeToDynamicPage(`space-application-page`, `${assistOS.space.id}/Space/document-view-page/${documentId}`);
-            }
-        )
-    }
-
-}
-*/
-
 const applicationModule = require('assistos').loadModule('application', {});
-const utilModule = require('assistos').loadModule('util', {}); // Importă utilModule
+const utilModule = require('assistos').loadModule('util', {});
 
 export class BooksGeneratorModal {
     constructor(element, invalidate) {
